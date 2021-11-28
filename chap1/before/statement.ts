5c25b467e3c4740960d7169271e4f735791a4148
miruoon <rillakerika@gmail.com>
2021-11-29 08:01:25 +0900

    Add chap1 sample

---
+++ chap1/before/statement.ts
@@ -0,0 +1,70 @@
+interface Performance {
+  playID: string;
+  audience: number;
+}
+
+interface Invoice {
+  customer: string;
+  performances: Performance[];
+}
+
+interface PlayData {
+  name: string;
+  type: string;
+}
+
+interface Play {
+  [key: string]: PlayData;
+}
+
+function statement(invoice: Invoice, plays: Play) {
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
+
+  for (const [_key, performance] of Object.entries(invoice.performances)) {
+    const play = plays[performance.playID];
+    let thisAmount = 0;
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (performance.audience > 30) {
+          thisAmount += 1000 * (performance.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (performance.audience > 20) {
+          thisAmount += 10000 + 500 * (performance.audience - 20);
+        }
+        thisAmount += 300 * performance.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+
+    volumeCredits += Math.max(performance.audience - 30, 0);
+    if ("comedy" === play.type) {
+      volumeCredits += Math.floor(performance.audience / 5);
+    }
+    result += `  ${play.name}: ${format(thisAmount / 100)} (${
+      performance.audience
+    } seats)\n`;
+    totalAmount += thisAmount;
+  }
+  result += `Amount owed is ${format(totalAmount / 100)}\n`;
+  result += `You earned ${volumeCredits} credits\n`;
+  return result;
+}
+
+// Use deno run --allow-read abc.ts
+const playsData = JSON.parse(Deno.readTextFileSync("../plays.json"));
+const invoicesData = JSON.parse(Deno.readTextFileSync("../invoices.json"));
+
+console.log(statement(invoicesData[0], playsData));
