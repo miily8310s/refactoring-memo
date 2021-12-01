bba5bc80d045aa2adf6e6fecc1fb255c85c2ac10
miruoon <rillakerika@gmail.com>
2021-12-02 06:23:52 +0900

    feat/chap1 html出力用メソッドを追加

--- chap1/after/stement_test.ts
+++ chap1/after/stement_test.ts
@@ -1,5 +1,5 @@
 import { assertEquals } from "https://deno.land/std@0.116.0/testing/asserts.ts";
-import { statement } from "./statement.ts";
+import { statement, htmlStatement } from "./statement.ts";
 
 Deno.test("statement test #1", () => {
   const testInvoices = {
@@ -31,5 +31,40 @@ Deno.test("statement test #1", () => {
   Othello: $500.00 (40 seats)
 Amount owed is $1,730.00
 You earned 47 credits\n`;
+  assertEquals(result, expect);
+});
+
+Deno.test("statement test #2", () => {
+  const testInvoices = {
+    customer: "BigCo",
+    performances: [
+      {
+        playID: "hamlet",
+        audience: 55,
+      },
+      {
+        playID: "as-like",
+        audience: 35,
+      },
+      {
+        playID: "othello",
+        audience: 40,
+      },
+    ],
+  };
+  const testPlays = {
+    hamlet: { name: "Hamlet", type: "tragedy" },
+    "as-like": { name: "As You like It", type: "comedy" },
+    othello: { name: "Othello", type: "tragedy" },
+  };
+  const result = htmlStatement(testInvoices, testPlays);
+  const expect = `<h1>Statement for BigCo</h1>
+<table>
+<tr><th>play</th><th>seats</th><th>const</th></tr></table>
+  <tr><td>Hamlet</td><td>55</td><td>$65,000.00</td></tr>
+  <tr><td>As You like It</td><td>35</td><td>$58,000.00</td></tr>
+  <tr><td>Othello</td><td>40</td><td>$50,000.00</td></tr>
+<p>Amount owed is <em>$173,000.00</em></p>
+<p>You earned <em>47</em> credits</p>\n`;
   assertEquals(result, expect);
 });
